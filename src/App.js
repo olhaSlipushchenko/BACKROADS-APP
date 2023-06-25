e1c236b98b377de201f5ad8dfa1ed81b754405af
olhaSlipushchenko <103112284+olhaSlipushchenko@users.noreply.github.com>
2023-06-25 13:04:54 +0300

    common

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Services from "./components/Services";
+import Tours from "./components/Tours";
+import Footer from "./components/Footer";
+import OpenLink from "./components/OpenLink";
+
+function App() {
+  return  (
+  <>
+    <Navbar />
+    <Hero />
+    <About />
+    <Services />
+    <Tours />
+    <Footer />
+    <OpenLink />
+  </>
+  );
+}
+
+export default App;
